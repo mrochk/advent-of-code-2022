bd0a20b911983aef387c3eab826d0f55fda746f0
Maxime Rochkoulets <mrochkoulets@gmail.com>
2022-12-05 21:23:41 +0100

    ..

--- day5/solution.go
+++ day5/solution.go
@@ -44,21 +44,11 @@ func (s *Stack) Pop() {
 	s.top = s.top.next
 }
 
-func (s *Stack) GetTop() byte {
+func (s *Stack) Val() byte {
 	if s.top != nil {
 		return s.top.content
 	}
 	return 0
-}
-
-func (s *Stack) Print() {
-	str := ""
-	temp := s.top
-	for temp != nil {
-		str += string(temp.content)
-		temp = temp.next
-	}
-	fmt.Println(str)
 }
 
 func getStacks() []*Stack {
@@ -91,16 +81,17 @@ func getStacks() []*Stack {
 	}
 	for i := range stacks {
 		for tempStacks[i].top != nil {
-			stacks[i].Top(tempStacks[i].GetTop())
+			stacks[i].Top(tempStacks[i].Val())
 			tempStacks[i].Pop()
 		}
 	}
 	return stacks
 }
 
-func part1() string {
+func solution() (string, string) {
 	var (
-		stacks   = getStacks()
+		stacks1  = getStacks()
+		stacks2  = getStacks()
 		input, _ = os.Open("input.txt")
 		reader   = bufio.NewReader(input)
 	)
@@ -116,24 +107,35 @@ func part1() string {
 			break
 		}
 		line = strings.ReplaceAll(line, "\n", "")
-		args := strings.Split(line, " ")
-		move, _ := strconv.Atoi(args[1])
-		from, _ := strconv.Atoi(args[3])
-		to, _ := strconv.Atoi(args[5])
-		from = from - 1
-		to = to - 1
+		var (
+			args      = strings.Split(line, " ")
+			move, _   = strconv.Atoi(args[1])
+			from, _   = strconv.Atoi(args[3])
+			to, _     = strconv.Atoi(args[5])
+			tempStack = NewStack()
+		)
+		from, to = from-1, to-1
+		for i := move; i > 0; i-- {
+			tempStack.Top(stacks2[from].Val())
+			stacks2[from].Pop()
+		}
 		for ; move > 0; move-- {
-			stacks[to].Top(stacks[from].GetTop())
-			stacks[from].Pop()
+			stacks1[to].Top(stacks1[from].Val())
+			stacks1[from].Pop()
+			stacks2[to].Top(tempStack.Val())
+			tempStack.Pop()
 		}
 	}
-	res := ""
-	for i := range stacks {
-		res += string(stacks[i].GetTop())
+	res1, res2 := "", ""
+	for i := range stacks1 {
+		res1 += string(stacks1[i].Val())
+		res2 += string(stacks2[i].Val())
 	}
-	return res
+	return res1, res2
 }
 
 func main() {
-	fmt.Println("Solution to part one =>", part1())
+	p1, p2 := solution()
+	fmt.Println("Solution to part one =>", p1)
+	fmt.Println("Solution to part one =>", p2)
 }
