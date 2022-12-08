2e160924f119e7c8cdd05894dd96ec13bd702344
Maxime Rochkoulets <mrochkoulets@gmail.com>
2022-12-08 22:10:34 +0100

    day8 1&2

--- day8/solution.py
+++ day8/solution.py
@@ -1,16 +1,10 @@
 input = open("input.txt", "r")
-
-# ligne, colonne
 grid = []
 while True:
-    line = input.readline().strip()
-    if line == "":
-        break
-    grid.append(line)
-
-visible_trees = 0
-visbl = []
-line_index = -1
+    line = input.readline()
+    if line == "": break
+    grid.append(line.strip())
+visible_trees, max_scenic_score, line_index = 0, 0, -1
 for line in grid:
     line_index += 1
     column_index = -1
@@ -18,53 +12,39 @@ for line in grid:
         column_index += 1
         if line_index in [0, len(grid)-1] or column_index in [0, len(line)-1]:
             visible_trees += 1
-            visbl.append(number)
         else:
-            X = False
-            
-            #check left
-            visible = True
+            is_tree_visible = False
+            visible, vdist_left = True, 0
             for i in range(column_index-1, -1, -1):
+                vdist_left += 1
                 if int(line[i]) >= int(number):
                     visible = False
                     break
-
-            if visible:
-                X = True
-
-            #check right
-            visible = True
+            if visible: is_tree_visible = True
+            visible, vdist_right = True, 0
             for i in range(column_index+1, len(line)):
+                vdist_right += 1
                 if int(line[i]) >= int(number):
                     visible = False
                     break
-
-            if visible:
-                X = True
-
-            #check top
-            visible = True
+            if visible: is_tree_visible = True
+            visible, vdist_top = True, 0
             for i in range(line_index-1, -1, -1):
-                if number == "1":
-                    print(f"checking {grid[i][column_index]}")
+                vdist_top += 1
                 if int(grid[i][column_index]) >= int(number):
                     visible = False
-                    
-            if visible:
-                X = True
-
-            #check bottom
-            visible = True
+                    break
+            if visible: is_tree_visible = True
+            visible, vdist_bottom = True, 0
             for i in range(line_index+1, len(grid)):
+                vdist_bottom += 1
                 if int(grid[i][column_index]) >= int(number):
                     visible = False
+                    break
+            if visible: is_tree_visible = True
+            if is_tree_visible: visible_trees += 1
+            scenic_score = vdist_left * vdist_right * vdist_bottom * vdist_top
+            if scenic_score > max_scenic_score: max_scenic_score = scenic_score 
 
-            if visible:
-                X = True
-            
-            if X:
-                visible_trees += 1
-                visbl.append(number)
-
-print(visbl)
 print(f"Solution to part one => {visible_trees}")
+print(f"Solution to part two => {max_scenic_score}")
